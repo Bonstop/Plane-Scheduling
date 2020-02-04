fbf271c87846a89e1db9f2f9d1c310bcb164d9cd
Bonstop <33862875+Bonstop@users.noreply.github.com>
2020-02-04 16:18:01 +0800

    Update RL_Core.py

--- RL_Core.py
+++ RL_Core.py
@@ -10,24 +10,26 @@ class RL(object):
         self.epsilon = e_greedy
         self.sarsa_table = pd.DataFrame(columns = self.actions , dtype = np.float64)
 
-
     # 选择动作
     def choose_action(self , observation):
+        #检查动作
         self.check_and_update(observation)
+
+        #根据概率选择数据
         if np.random.rand() < self.epsilon:
-            #  == 返回该数据的index的数值，然后挑一个选择 ==
             state_index_list = self.sarsa_table[0].tolist()
             k = state_index_list.index(observation)
             state_action = self.sarsa_table.loc[k , 1:]
             action = np.random.choice(state_action[state_action == np.max(state_action)].index) # 随机找一个最大的
         else:
-            action = np.random.choice(self.actions[1:]) # 其他的随即找
+            action = np.random.choice(self.actions[1:]) # 其他的随机找
         return action
 
     #检查并更新 Sarsa表
     def check_and_update(self , state):
         if state not in self.sarsa_table[0].tolist():
-            self.sarsa_table = self.sarsa_table.append({0 : state , 1 : 0 , 2 : 0} , ignore_index = True)
+
+            self.sarsa_table = self.sarsa_table.append({0 : state , 1 : 0 , 2 : 0 , 3 : 0 , 4 : 0} , ignore_index = True)
 
 
 class SarsaTable(RL):
@@ -44,12 +46,9 @@ class SarsaTable(RL):
         plane_row = self.sarsa_table[0].tolist()
         s_index = plane_row.index(s)
         next_s_index = plane_row.index(next_s)
-
         sarsa_predict = self.sarsa_table.loc[s_index , a]
-
         if r <= 0:
             sarsa_target = r + self.gamma * self.sarsa_table.loc[next_s_index , 1:].max()
         else:
             sarsa_target = r
         self.sarsa_table.loc[s_index , a] += self.lr * (sarsa_target - sarsa_predict)
-        print(self.sarsa_table)
